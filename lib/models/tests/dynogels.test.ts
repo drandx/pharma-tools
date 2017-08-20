d186a96db60db370fa387234cc81c232c6826e06
Juan Pablo Garcia <garjuanpablo@gmail.com>
2017-08-20 01:48:31 -0500

    Minor changes

--- lib/models/tests/dynogels.test.ts
+++ lib/models/tests/dynogels.test.ts
@@ -1,11 +1,17 @@
 import { QuotePharmacy } from '../QuotePharmacy';
 import { Utils } from '../../config/utils';
 import { Quote } from '../Quote';
+import { Order } from '../Order';
+import { Product } from '../Product';
 
 const quotePharmacy: QuotePharmacy = new QuotePharmacy();
 let quote: QuotePharmacy = new QuotePharmacy();
+let order: Order = new Order();
 
 describe('Adds a random element to the DB', () => {
+    const product: Product = new Product();  
+    product.id = 'testing-product';
+    
     beforeAll(done => {
         quotePharmacy.model.create({
             quoteId: 'test-dynogels-types-quoteId',
@@ -17,6 +23,22 @@ describe('Adds a random element to the DB', () => {
             quote.quoteId = res.get().quoteId;
             console.log('Before all Res: ', res.get(), quote.quoteId, quote.pharmacyId);            
             done();
+        });
+    });
+
+    beforeAll(done => {
+        order.model.create(
+            {
+                id: order.id,
+                customerId: '595903fc83f7ab7331625bd1',
+                pharmacyId: 'd2855612-1f15-4cbe-9743-e089638f3d4b',
+                quoteId: '1ea3a2cd-3797-4bad-aaf3-2073445cefa4',
+                products: [product],
+            },{},
+            (err, res) => {
+                order = res.get();
+                console.log('Before all create order: ', order);
+                done();
         });
     });
 
@@ -57,9 +79,17 @@ describe('Adds a random element to the DB', () => {
     });
 
     afterAll(() => {
-        console.log('After all: ', quote.quoteId, quote.pharmacyId);
         quote.model.destroy(quote.quoteId, quote.pharmacyId, (err) => {
             expect(err).toBeUndefined();
+            console.log('**Deleted quote: ', quote);
+        })
+    })
+
+    afterAll(() => {
+        quote.model.destroy(order.id, order.customerId, (err) => {
+            expect(err).toBeUndefined();
+            console.log('**Err: ', err);
+            console.log('Deleted order: ', order);
         })
     })
 });
