bd0f49aa942c26d1aadaff15fcf2372abcb38a0b
darghexpharma <darwin@pharmalatam.com>
2017-09-03 08:08:36 -0500

    Mapping models

--- lib/models/Prescription.ts
+++ lib/models/Prescription.ts
@@ -3,8 +3,31 @@ import { BaseModel } from './BaseModel';
 
 export class Prescription extends BaseModel {
   public id: string;
+  public name: string;
+
+
   constructor() {
     super();
     this.id = uuid.v4();
   }
+
+  public model: dynogels.Model = dynogels.define(`${globalConst.stage}_prescriptions`, {
+    hashKey: 'id',
+    rangeKey: 'createdAt',
+    timestamps: false,
+    createdAt: false,
+    schema: {
+      id: joi.string(),
+      doctor: joi.object(),
+      patient: joi.string().email(),
+      formula: joi.array().items(joi.object()),
+      createdAt: joi.number(),
+      updatedAt: joi.number(),
+    },
+    tableName: `${globalConst.stage}_users`,
+    indexes : [
+      { hashKey : 'doctor.id', rangeKey : 'createAt', type : 'global', name : 'doctorIndex',},
+      { hashKey : 'patient.id', rangeKey : 'createAt', type : 'global', name : 'patientIndex',},
+      ]
+});
 }
--- lib/models/Product.ts
+++ lib/models/Product.ts
@@ -3,8 +3,24 @@ import { BaseModel } from './BaseModel';
 
 export class Product extends BaseModel {
   public id: string;
+  public name: string;
+
   constructor() {
     super();
     this.id = uuid.v4();
   }
+
+  public model: dynogels.Model = dynogels.define(`${globalConst.stage}_users`, {
+    hashKey: 'id',
+    rangeKey: 'name',
+    timestamps: false,
+    createdAt: false,
+    schema: {
+      id: joi.string(),
+      name: joi.string(),
+      createdAt: joi.number(),
+      updatedAt: joi.number(),
+    },
+    tableName: `${globalConst.stage}_products`
+});
 }
