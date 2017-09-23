ca7174a71fce904425e7532e87b94ccf2095e3c6
Jhon Darwin Muñoz Lopez <darwin@pharmalatam.com>
2017-09-22 22:39:40 -0500

    Include appVariables AWS to module

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,6 @@
 export * from './models';
 export * from './config/utils';
 export * from './config/db/db';
+export * from './config/db/appVariables';
 export * from './state-machines';
 export * from './services';
