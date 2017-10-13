08aeab74d9b83ef0183de5a8e6cd95719a885e75
Juan Pablo Garcia <garjuanpablo@gmail.com>
2017-10-13 15:59:04 -0500

    Fixed issue with PUSH_NOTIFICATIONS_TOPICS type

--- lib/models/Enums.ts
+++ lib/models/Enums.ts
@@ -34,13 +34,14 @@ export enum STATUS_ENUM {
     INACTIVE = 'INACTIVE',
 }
 
-export const PUSH_NOTIFICATIONS_ENUM = {
-    PATIENT_NEW_QUOTE_ESTIMATED: {
-        type: 'PATIENT_NEW_QUOTE_ESTIMATED',
-        arn: `arn:aws:sns:${awsConfig.region}:${awsConfig.arn}:${globalConst.stage}-push-notifications`,
-    },
-    PATIENT_NOT_AVAILABLE_STORES: {
-        type: 'PATIENT_NOT_AVAILABLE_STORES',
-        arn: '',
-    }   
+export class PUSH_NOTIFICATIONS_TOPICS {
+    public static PATIENT_NEW_QUOTE_ESTIMATED: PUSH_NOTIFICATIONS_TOPICS = new PUSH_NOTIFICATIONS_TOPICS('PATIENT_NEW_QUOTE_ESTIMATED', `arn:aws:sns:${awsConfig.region}:${awsConfig.arn}:${globalConst.stage}-push-notifications`);
+    public static PATIENT_NOT_AVAILABLE_STORES: PUSH_NOTIFICATIONS_TOPICS = new PUSH_NOTIFICATIONS_TOPICS('PATIENT_NOT_AVAILABLE_STORES', '');    
+    
+    constructor(public type: string, public arn: string) {
+    }
+    
+    public toString(): string {
+        return this.type + this.arn;
+    }
 }
--- lib/models/PushNotification.ts
+++ lib/models/PushNotification.ts
@@ -2,7 +2,7 @@ import * as uuid from 'uuid';
 import * as dynogels from 'drandx-dynogels';
 import { BaseModel } from './BaseModel';
 import { globalConst } from '../config/db/appVariables';
-import { PUSH_NOTIFICATIONS_ENUM } from './Enums';
+import { PUSH_NOTIFICATIONS_TOPICS } from './Enums';
 
 export interface PayLoad {
     [key: string]: (string
@@ -13,7 +13,7 @@ export interface PayLoad {
   }
 
 export class PushNotification extends BaseModel {
-    public pushType: PUSH_NOTIFICATIONS_ENUM;
+    public pushType: PUSH_NOTIFICATIONS_TOPICS;
     public userId: string;
     public payLoad: PayLoad;
 
